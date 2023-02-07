6056bec86e5cb95a6a0a8566a9f6a52603302ad1
duanpengpeng <duanpengpeng@wondertek.com.cn>
2023-02-07 21:57:58 +0800

    数组中重复数字查找问题1

--- src/main/java/com/dpp/array/FindDuplicateNum1.java
+++ src/main/java/com/dpp/array/FindDuplicateNum1.java
@@ -1,7 +1,5 @@
 package com.dpp.array;
 
-import java.util.HashSet;
-
 /**
  * 在一个长度为n的数组里的所有数字都在0~n-1的范围内。数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。
  * 例如，如果输入长度为7的数组{2，3，1，0，2，5，3}，那么对应的输出是重复的数字2或者3。
@@ -29,13 +27,14 @@ public class FindDuplicateNum1 {
         if (length == 1) {
             return -1;
         }
-        HashSet<Integer> numSet = new HashSet<>(length);
-        for (int v : arr) {
-            if (numSet.contains(v)) {
-                return v;
-            } else {
-                numSet.add(v);
+        //数组arr的数字都在0~length-1范围内
+        int[] table = new int[length];
+        for (int i = 0; i < length; i++) {
+            int num = arr[i];
+            if (table[num] > 0) {
+                return num;
             }
+            table[num] = 1;
         }
         return -1;
     }
@@ -43,6 +42,7 @@ public class FindDuplicateNum1 {
     /**
      * 不使用额外存储，O(1)复杂度
      * 数组中的数字都在0~n-1的范围内，如果这个数组中没有重复的数字，那么当数组排序之后数字i将出现在下标为i的位置。
+     *
      * @return
      */
     public static int findNum2(int[] arr) {
@@ -54,10 +54,10 @@ public class FindDuplicateNum1 {
         }
         for (int i = 0; i < arr.length; i++) {
             int num = arr[i];
-            while (num != i){
-                if (num == arr[num]){
+            while (num != i) {
+                if (num == arr[num]) {
                     return num;
-                }else {
+                } else {
                     arr[i] = arr[num];
                     arr[num] = num;
                     num = arr[i];
