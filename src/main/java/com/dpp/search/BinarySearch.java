d8dae7617f3f2b7852ce844f7163fd728f726867
duanpengpeng <duanpengpeng@wondertek.com.cn>
2024-07-01 17:27:38 +0800

    二分查找算法

--- src/main/java/com/dpp/search/BinarySearch.java
+++ src/main/java/com/dpp/search/BinarySearch.java
@@ -1,5 +1,8 @@
 package com.dpp.search;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Supplier;
+
 /**
  * @author dpp
  * @date 2024/6/24
@@ -19,13 +22,6 @@ public class BinarySearch {
         }
         int left = 0;
         int right = arr.length - 1;
-        return doBinarySearch(arr, target, left, right);
-    }
-
-    private static int doBinarySearch(int[] arr, int target, int left, int right) {
-        if (arr == null || arr.length == 0 || left > right) {
-            return -1;
-        }
         while (left <= right) {
             int mid = left + (right - left) / 2;
             if (arr[mid] == target) {
@@ -38,7 +34,6 @@ public class BinarySearch {
         }
         return -1;
     }
-
 
     /**
      * 查找数组中第一个值等于target的元素的位置
@@ -54,14 +49,21 @@ public class BinarySearch {
         }
         int left = 0;
         int right = arr.length - 1;
-        int result = -1;
-        int index = doBinarySearch(arr, target, left, right);
-        while (index >= 0) {
-            result = index;
-            //继续在左边寻找是否有等于target的元素
-            index = doBinarySearch(arr, target, left, index - 1);
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                if (mid == 0 || arr[mid - 1] != target) {
+                    return mid;
+                } else {
+                    right = mid - 1;
+                }
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
         }
-        return result;
+        return -1;
     }
 
     /**
@@ -78,14 +80,21 @@ public class BinarySearch {
         }
         int left = 0;
         int right = arr.length - 1;
-        int result = -1;
-        int index = doBinarySearch(arr, target, left, right);
-        while (index >= 0) {
-            result = index;
-            //继续在左边寻找是否有等于target的元素
-            index = doBinarySearch(arr, target, index + 1, right);
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                if (mid == arr.length - 1 || arr[mid + 1] != target) {
+                    return mid;
+                } else {
+                    left = mid + 1;
+                }
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
         }
-        return result;
+        return -1;
     }
 
     /**
@@ -106,20 +115,18 @@ public class BinarySearch {
         }
         int left = 0;
         int right = arr.length - 1;
-        int firstGteIndex = -1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (arr[mid] == target) {
-                firstGteIndex = mid;
+            if (arr[mid] >= target) {
+                if (mid == 0 || arr[mid - 1] < target) {
+                    return mid;
+                }
                 right = mid - 1;
-            } else if (arr[mid] < target) {
-                left = mid + 1;
             } else {
-                right = mid - 1;
-                firstGteIndex = mid;
+                left = mid + 1;
             }
         }
-        return firstGteIndex;
+        return -1;
     }
 
     /**
@@ -127,8 +134,6 @@ public class BinarySearch {
      * 比如：arr=[1,3,3,3,3,4,6],target=1,则返回index=0
      * 比如：arr=[1,3,3,3,3,4,6],target=4,则返回index=4
      * 思路：
-     * 1.找到最后一个值小于等于target的元素位置，如果有，则index+1就是第一个；
-     * 2.如果没找到等于target的元素，则需要记录下第一次比他大的元素位置
      *
      * @param arr
      * @param target
@@ -140,20 +145,18 @@ public class BinarySearch {
         }
         int left = 0;
         int right = arr.length - 1;
-        int lastLteIndex = -1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (arr[mid] == target) {
-                lastLteIndex = mid;
+            if (arr[mid] <= target) {
+                if (mid == arr.length - 1 || arr[mid + 1] > target) {
+                    return mid;
+                }
                 left = mid + 1;
-            } else if (arr[mid] < target) {
-                left = mid + 1;
-                lastLteIndex = mid;
             } else {
                 right = mid - 1;
             }
         }
-        return lastLteIndex;
+        return -1;
     }
 
     private static void testFirstEq() {
@@ -187,12 +190,12 @@ public class BinarySearch {
     }
 
     public static void main(String[] args) {
-//        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-//        int index = binarySearch(arr, 5);
-//        System.out.println("Found target at index: " + index);
-//        testFirstEq();
-//        testLastEq();
-        //testFirstGte();
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int index = binarySearch(arr, 5);
+        System.out.println("Found target at index: " + index);
+        testFirstEq();
+        testLastEq();
+        testFirstGte();
         testLastLte();
     }
 
