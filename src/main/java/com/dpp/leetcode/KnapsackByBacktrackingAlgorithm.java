bb5afa397505489e6067477a11584a6a7c908606
duanpengpeng <duanpengpeng@wondertek.com.cn>
2024-08-12 10:29:30 +0800

    回溯算法解决0-1背包问题

--- src/main/java/com/dpp/leetcode/KnapsackByBacktrackingAlgorithm.java
+++ src/main/java/com/dpp/leetcode/KnapsackByBacktrackingAlgorithm.java
@@ -8,9 +8,9 @@ package com.dpp.leetcode;
 public class KnapsackByBacktrackingAlgorithm {
 
 
-    private static int maxW = Integer.MIN_VALUE;
+    private static int MAX_PRICE = Integer.MIN_VALUE;
 
-    private static int[] bestx;
+    private static int WEIGHT = Integer.MIN_VALUE;
 
     /**
      * @param i      i表示考察到哪个物品了；
@@ -19,30 +19,32 @@ public class KnapsackByBacktrackingAlgorithm {
      * @param n      n表示物品个数
      * @param m      w背包重量
      */
-    public static void f(int i, int cw, int[] weight, int n, int m) {
+    public static void f(int i, int cw, int maxPrice, int[] weight, int[] price, int n, int m) {
         //物品都计算完成或总重量等于背包容量时，退出
         if (i == n || cw == m) {
-            if (cw > maxW) {
-                maxW = cw;
+            if (maxPrice > MAX_PRICE) {
+                WEIGHT = cw;
+                MAX_PRICE = maxPrice;
             }
             return;
         }
         //当前物品不装入背包时，计算可装入的总重量
-        f(i + 1, cw, weight, n, m);
+        f(i + 1, cw, maxPrice, weight, price, n, m);
         if (cw + weight[i] <= m) {
-            //当前物品装入背包 cw+weight[i]，计算可装入的总重量
-            f(i + 1, cw + weight[i], weight, n, m);
+            //当前物品装入背包 cw+weight[i]，计算可装入的价值
+            f(i + 1, cw + weight[i], maxPrice + price[i], weight, price, n, m);
         }
 
     }
 
     public static void main(String[] args) {
-        int[] weight = {25, 40, 30, 20}; // 物品重量
-        int m = 100; // 背包=包容量
+        int[] weight = {12, 2, 1, 1, 4}; // 物品重量kg
+        int[] price = {4, 2, 2, 1, 10};  // 物品价值
+        int m = 15; // 背包=包容量 kg
         int n = weight.length; // 物品个数
 
-        f(0, 0, weight, n, m);
+        f(0, 0, 0, weight, price, n, m);
 
-        System.out.println(maxW);
+        System.out.println("背包可装物品的总价值最大是 = " + MAX_PRICE+", 总重量= " + WEIGHT + "kg");
     }
 }
